30db4cd1030cd446242e6e09865484c8274a63b9
Krish <krishkapadia00@gmail.com>
2021-03-05 16:16:18 +0530

    ios splash screen done,card modal design change

---
+++ ios/Dynamic.swift
@@ -0,0 +1,8 @@
+//
+//  Dynamic.swift
+//  PracticeApp
+//
+//  Created by Krish on 05/03/21.
+//
+
+import Foundation
--- ios/PracticeApp-Bridging-Header.h
+++ ios/PracticeApp-Bridging-Header.h
@@ -1,4 +1,12 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  PracticeApp-Bridging-Header.h
+//  PracticeApp
+//
+//  Created by Krish on 05/03/21.
 //
 
+#ifndef PracticeApp_Bridging_Header_h
+#define PracticeApp_Bridging_Header_h
+
+
+#endif /* PracticeApp_Bridging_Header_h */
